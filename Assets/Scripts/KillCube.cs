e8567a48a67cef678e1d910f03b805d01e31fa8b
Elias Heijari <69460338+elkkuzq@users.noreply.github.com>
2023-11-15 12:42:16 +0200

    KillCube Animation Start Randomly in 3 sec

--- Assets/Scripts/KillCube.cs
+++ Assets/Scripts/KillCube.cs
@@ -8,12 +8,19 @@ public class KillCube : MonoBehaviour
     [SerializeField] private GameObject destroyParticle;
     private void Start()
     {
+        StartCoroutine(PlayAnimationAfterTime());
+    }
+
+    IEnumerator PlayAnimationAfterTime()
+    {
+        float time = Random.Range(0f, 3f);
+        yield return new WaitForSeconds(time);
         int animNum = Random.Range(0, 3);
         animator.speed = Random.Range(0.1f, 0.7f);
         if (animNum > 1)
         {
             animator.Play("KillCubeMovement");
-            
+
         }
         else
         {
